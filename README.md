62a690bddcb38c183c8ad410bf4b4c72668e9d4a
Your Name <you@example.com>
2018-04-29 20:21:23 +0100

    automatization of md engine

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # duarte-pompeu.github.io
 My personal gh-page, using pompas_md.
+
+Markdown reference taken from http://spec.commonmark.org/0.28/
--- md_all.sh
+++ md_all.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+set -x #echo on
 
+rm -rf ./blog/*
 cd source/
+
+FOLDERS=$(find . -type d)
+
+for f in $FOLDERS; do
+	mkdir ../blog/$f
+done
 
 IFS=$'\n'
 for markdown in $(find . | grep -E "\.md$"); do
-	echo $markdown
-	../pompas_md.py $markdown ../blog 
+	# some path manipulation to turn "./folder/example.md" into "folder/example.md"
+	file=$(echo $markdown | sed 's/^.\///')
+	
+	../pompas_md.py $file ../blog
 done
+
+
+
+
+
+
